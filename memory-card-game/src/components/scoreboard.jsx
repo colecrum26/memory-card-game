e65946fb2fed43cb48cdfdadb2c211892dc86de5
colecrum26 <cwcrumrine@gmail.com>
2023-04-08 20:24:14 -0500

    updated the mission

--- memory-card-game/src/components/scoreboard.jsx
+++ memory-card-game/src/components/scoreboard.jsx
@@ -3,7 +3,7 @@ function ScoreKeeper({ scoreCurr, scoreBest }) {
     <header className="App-header">
       <div>
         <h1 id="heading">Test Your Memory!</h1>
-        <p id="mission">Click on the matching items as they appear.</p>
+        <p id="mission">Click on the items you've seen before as they appear.</p>
         <div className ="d-flex justify-content-around align-items-center">
           <div>
             <p>Current Score:</p>
